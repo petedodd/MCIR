b2c8cc4147f655c15939f6054037416031a54a38
PJ Dodd <p.j.dodd@sheffield.ac.uk>
2020-01-08 19:49:42 +0000

    startup warning

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onAttach <- function(libname='MCIR',pkgname='MCIR'){
+    packageStartupMessage("PLEASE NOTE!\n Incomplete and not yet licenced for use (see https://github.com/petedodd/MCIR).")
+}
+
